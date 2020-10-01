f6738becfef628d6c10ea378d86904fcf8f8b953
Joseph Castle <joseph.castle@gsa.gov>
2020-09-30 23:39:09 -0400

    Updates to mole game

--- Build_Game-Feed-Mole/mole.js
+++ Build_Game-Feed-Mole/mole.js
@@ -1,1 +1,67 @@
-console.log('it works!')
+function getSadInterval() {
+    return Date.now() + 1000;
+}
+
+const moles = [
+    {
+        status: 'sad',
+        next: getSadInterval(),
+        king: false,
+        node: document.querySelector('#hole-0')
+    },
+    {
+        status: 'sad',
+        next: getSadInterval(),
+        king: false,
+        node: document.querySelector('#hole-1')
+    },
+    {
+        status: 'sad',
+        next: getSadInterval(),
+        king: false,
+        node: document.querySelector('#hole-2')
+    },
+    {
+        status: 'sad',
+        next: getSadInterval(),
+        king: false,
+        node: document.querySelector('#hole-3')
+    },
+    {
+        status: 'sad',
+        next: getSadInterval(),
+        king: false,
+        node: document.querySelector('#hole-4')
+    },
+    {
+        status: 'sad',
+        next: getSadInterval(),
+        king: false,
+        node: document.querySelector('#hole-5')
+    },
+    {
+        status: 'sad',
+        next: getSadInterval(),
+        king: false,
+        node: document.querySelector('#hole-6')
+    },
+    {
+        status: 'sad',
+        next: getSadInterval(),
+        king: false,
+        node: document.querySelector('#hole-7')
+    },
+    {
+        status: 'sad',
+        next: getSadInterval(),
+        king: false,
+        node: document.querySelector('#hole-8')
+    },
+    {
+        status: 'sad',
+        next: getSadInterval(),
+        king: false,
+        node: document.querySelector('#hole-9')
+    }
+];
+
