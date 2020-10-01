1d311c05aa1cb9ccf4c17a0478667bf80b7ca01e
Joseph Castle <joseph.castle@gsa.gov>
2020-10-01 00:04:10 -0400

    Updates to game

--- Build_Game-Feed-Mole/mole.js
+++ Build_Game-Feed-Mole/mole.js
@@ -2,6 +2,10 @@ function getSadInterval() {
     return Date.now() + 1000;
 }
 
+function getGoneInterval() {
+    return Date.now() + Math.floor(Math.random() * 18000) + 2000;
+}
+
 const moles = [
     {
         status: 'sad',
@@ -65,3 +69,34 @@ const moles = [
     }
 ];
 
+function getNextStatus(mole) {
+    switch (mole.status) {
+        case 'sad':
+            mole.next = getSadInterval();
+            mole.status = 'leaving';
+            mole.node.src = './images/mole-leaving.png'
+            break;
+        case 'leaving':
+            mole.next = getGoneInterval();
+            mole.status = 'gone';
+            mole.node.children[0].classList.add('gone');
+            break;
+    }
+}
+
+
+let runAgainAt = Date.now() + 100;
+function nextFrame() {
+    const now = Date.now();
+
+    if (runAgainAt <= now) {
+        for (let i = 0; i < moles.length; i++) {
+            if (moles[i].next <= now) {
+                getNextStatus(moles[i]);
+            }
+        }
+        runAgainAt = now + 100;
+    }
+    requestAnimationFrame(nextFrame);
+}
+nextFrame();
