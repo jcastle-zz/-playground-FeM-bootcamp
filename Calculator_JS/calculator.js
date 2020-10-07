518d0c5b35ca46a5003e89d4779300239a746f3d
Joseph Castle <joseph.castle@gsa.gov>
2020-10-07 09:55:24 -0400

    Updates to the calculator JS

--- Calculator_JS/calculator.js
+++ Calculator_JS/calculator.js
@@ -5,3 +5,93 @@ Requirements:
 3. previousOperator - Need to keep state, keep track of previous actions. Represents operator (+, -, =).
 
 */
+
+let runningTotal = 0;
+let buffer = '0';
+let previousOperator;
+
+const screen = document.querySelector('.screen');
+
+// Something happens when clicking a button
+function buttonClick(value) {
+    if (isNaN(value)) {
+        // this is not a number
+        handleSymbol(value);
+    } else {
+        // this is a number
+        handleNumber(value);
+    }
+    screen.innerText = buffer;
+}
+
+function handleSymbol(symbol){
+    console.log('handleSymbol', symbol);
+    switch (symbol) {
+        case 'C':
+            buffer = '0';
+            runningTotal = '0';
+            break;
+        // addition
+        case '&plus;':
+        // subtraction
+        case '&minus;':
+        // multiplication
+        case '&times;':
+        // division
+        case '&divide;':
+            handleMath(symbol);
+            break;
+    }
+}
+
+function handleMath(symbol) {
+    if (buffer === '0') {
+        // do nothing
+        return;
+    } 
+
+    // parseInt(buffer) could also be written as +buffer
+    const intBuffer = parseInt(buffer);
+
+    if (runningTotal === 0) {
+        runningTotal = intBuffer;
+    } else {
+        flushOperation(intBuffer);
+    }
+
+    previousOperator = symbol;
+    
+    buffer = '0';
+}
+
+function flushOperation(intBuffer) {
+    if (previousOperator === '+') {
+        runningTotal += intBuffer;
+    } else if (previousOperator === '−') {
+        runningTotal -+ intBuffer;
+    } else if (previousOperator === '×') {
+        runningTotal *= intBuffer;
+    } else {
+        runningTotal /= intBuffer;
+    }
+
+}
+
+function handleNumber(numberString) {
+    if (buffer === '0') {
+        buffer = numberString;
+    } else {
+        // buffer = buffer + numberString;
+        buffer += numberString;
+    }
+}
+
+// This function get's called once and set's everything up
+function init() {
+    document.querySelector('.calc-buttons')
+    .addEventListener('click', function (e) {
+        buttonClick(e.target.innerText);
+    })
+}
+
+init();
